d1d397b0bfcf4601063caceccac11e26383136dd
harafjakub <harafjakub@gmail.com>
2024-03-18 06:52:10 +0100

    Remove redundant code

--- src/zad4/script.js
+++ src/zad4/script.js
@@ -6,10 +6,6 @@ $(document).ready(function() {
         $(this).css('background-color', '');
         $(this).find('a').css('color', '#333');
     });
-});
-
-
-$(document).ready(function() {
 
     function displayProducts(category, page) {
         $.ajax({
@@ -37,29 +33,5 @@ $(document).ready(function() {
         var category = $("#category").val();
         var page = $(this).attr("data-page");
         displayProducts(category, page);
-    });
-});
-
-
-$(document).ready(function() {
-    function displayProducts(category, page) {
-        $.ajax({
-            url: "products.php",
-            type: "GET",
-            data: { category: category, page: page },
-            success: function(response) {
-                $("#product-list").html(response);
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
-    }
-    
-    displayProducts("", 1);
-
-    $("#category").change(function() {
-        var category = $(this).val();
-        displayProducts(category, 1);
     });
 });
